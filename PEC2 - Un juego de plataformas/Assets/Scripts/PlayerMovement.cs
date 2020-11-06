c42e583452e1885ffe998809286075ec93aa96b9
Javier <mrvizious@gmail.com>
2020-11-06 17:04:00 +0100

    Jump on walls fixed, blocks changed to gameobjects instead of tilemap

--- PEC2 - Un juego de plataformas/Assets/Scripts/PlayerMovement.cs
+++ PEC2 - Un juego de plataformas/Assets/Scripts/PlayerMovement.cs
@@ -10,6 +10,7 @@ public class PlayerMovement : MonoBehaviour
     public float groundHorizontalAcceleration = 500f, airHorizontalAcceleration = 200f, maxHorizontalSpeed = 3.7f;
     public float groundCheckDistance = 0.05f, jumpForce = 17.36f;
     public float normalGravity = 6.9f, slowGravity = 3.5f;
+    public float groundCheckOffset = 0.625f;
     public bool grounded = false;
     private int groundCheckLayermask;
     void Start()
@@ -28,20 +29,26 @@ public class PlayerMovement : MonoBehaviour
             grounded = false;
         }
 
-        //Add speed if moving on the ground
-        if (input.horizontalInput != 0f)
-        {
-            if (grounded) rb.velocity = new Vector2(rb.velocity.x + input.horizontalInput * groundHorizontalAcceleration * Time.deltaTime, rb.velocity.y);
-            else rb.velocity = new Vector2(rb.velocity.x + input.horizontalInput * airHorizontalAcceleration * Time.deltaTime, rb.velocity.y);
-        }
+        //Debug.Log("X velocity: " + rb.velocity.x);
+
+        //Add speed if on the ground
+        if (grounded) rb.velocity = new Vector2(rb.velocity.x + input.horizontalInput * groundHorizontalAcceleration * Time.deltaTime, rb.velocity.y);
+        else rb.velocity = new Vector2(rb.velocity.x + input.horizontalInput * airHorizontalAcceleration * Time.deltaTime, rb.velocity.y);
     }
     void FixedUpdate()
     {
         //Check ground
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, -rb.transform.up, groundCheckDistance, groundCheckLayermask);
-        Debug.DrawRay(transform.position, -rb.transform.up * groundCheckDistance, Color.red);
-        if (hit.collider != null) grounded = hit.collider.tag.Equals("Terrain");
-        else grounded = false;
+        RaycastHit2D hitLeft = Physics2D.Raycast(transform.position - ((Vector3)Vector2.right * groundCheckOffset), -rb.transform.up, groundCheckDistance, groundCheckLayermask);
+        Debug.DrawRay(transform.position - ((Vector3)Vector2.right * groundCheckOffset), -rb.transform.up * groundCheckDistance, Color.red);
+        RaycastHit2D hitRight = Physics2D.Raycast(transform.position + ((Vector3)Vector2.right * groundCheckOffset), -rb.transform.up, groundCheckDistance, groundCheckLayermask);
+        Debug.DrawRay(transform.position + ((Vector3)Vector2.right * groundCheckOffset), -rb.transform.up * groundCheckDistance, Color.red);
+
+        bool groundedLeft = false;
+        bool groundedRight = false;
+
+        if (hitLeft.collider != null) groundedLeft = hitLeft.collider.tag.Equals("Terrain") || hitLeft.collider.tag.Equals("Block");
+        if (hitRight.collider != null) groundedRight = hitRight.collider.tag.Equals("Terrain") || hitRight.collider.tag.Equals("Block");
+        grounded = groundedLeft || groundedRight;
 
         rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -maxHorizontalSpeed, maxHorizontalSpeed), rb.velocity.y);
 
