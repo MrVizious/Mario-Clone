0a40bfc04a0802b83886057b19acf15c776378b0
Javier <mrvizious@gmail.com>
2020-12-10 18:53:48 +0100

    UI

---
+++ PEC2 - Un juego de plataformas/Assets/Scripts/UI/HealthUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class HealthUI : MonoBehaviour
+{
+    public LevelData data;
+    private Text text;
+
+    private void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        text.text = "Lifes: " + data.currentNumberOfLifes;
+    }
+
+}
